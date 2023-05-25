f7bd69c0dfdc52343a8a9cdcba8243081be94b9f
Kenneth <kennethk.1203@gmail.com>
2023-05-26 02:08:08 +0800

    TypeScript fix

--- src/components/InputField/index.tsx
+++ src/components/InputField/index.tsx
@@ -1,6 +1,7 @@
 import { InputWrapper, InputComponent } from "./styles";
 
 interface PropTypes extends React.HTMLAttributes<HTMLInputElement> {
+  type?: "text" | "email" | "password" | "number";
   label?: string;
   htmlFor?: string;
   fullWidth?: boolean;
@@ -9,13 +10,14 @@ interface PropTypes extends React.HTMLAttributes<HTMLInputElement> {
 const InputField: React.FC<PropTypes> = ({
   label,
   htmlFor,
+  type,
   fullWidth = false,
   ...props
 }) => {
   return (
     <InputWrapper fullWidth={fullWidth}>
       {label && label}
-      <InputComponent {...props} />
+      <InputComponent type={type} {...props} />
     </InputWrapper>
   );
 };
